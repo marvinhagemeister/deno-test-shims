f4b7bc9db7c24200bba1991fea17e6e9e593b691
Marvin Hagemeister <marvin@deno.com>
2024-04-02 10:35:59 +0200

    fix: remove require shim

--- src/mod.ts
+++ src/mod.ts
@@ -1,6 +1,5 @@
 import process from "node:process";
 import { Buffer } from "node:buffer";
-import { createRequire } from "node:module";
 import { afterAll, beforeAll, it } from "@std/testing/bdd";
 import { fn } from "@std/expect";
 export * from "@std/testing/bdd";
@@ -10,9 +9,6 @@ export * from "@std/expect";
 globalThis.process = process;
 // @ts-ignore node shim
 globalThis.Buffer = Buffer;
-// @ts-ignore node shim, not entirely accurate but works well enough for
-// bare specifiers
-globalThis.require = createRequire(import.meta.url);
 
 export const after = afterAll;
 export const before = beforeAll;
