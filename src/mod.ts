9848a9b6b8a9288b07d3d5d147e76e57a0529039
Marvin Hagemeister <marvin@deno.com>
2024-04-02 10:19:35 +0200

    feat: shim require

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,6 @@
 import process from "node:process";
 import { Buffer } from "node:buffer";
+import { createRequire } from "node:module";
 import { afterAll, beforeAll } from "@std/testing/bdd";
 export * from "@std/testing/bdd";
 
@@ -7,6 +8,9 @@ export * from "@std/testing/bdd";
 globalThis.process = process;
 // @ts-ignore node shim
 globalThis.Buffer = Buffer;
+// @ts-ignore node shim, not entirely accurate but works well enough for
+// bare specifiers
+globalThis.require = createRequire(import.meta.url);
 
 export const after = afterAll;
 export const before = beforeAll;
