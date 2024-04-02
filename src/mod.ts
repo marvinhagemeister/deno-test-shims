e80d11cb77ac92a9ebad4b71153bbd56161f9360
Marvin Hagemeister <marvin@deno.com>
2024-04-02 10:32:33 +0200

    feat: shim vitest `vi`

--- src/mod.ts
+++ src/mod.ts
@@ -2,6 +2,7 @@ import process from "node:process";
 import { Buffer } from "node:buffer";
 import { createRequire } from "node:module";
 import { afterAll, beforeAll, it } from "@std/testing/bdd";
+import { fn } from "@std/expect";
 export * from "@std/testing/bdd";
 export * from "@std/expect";
 
@@ -12,6 +13,10 @@ globalThis.Buffer = Buffer;
 // @ts-ignore node shim, not entirely accurate but works well enough for
 // bare specifiers
 globalThis.require = createRequire(import.meta.url);
+// @ts-ignore shim for vitest
+globalThis.vi = {
+  fn,
+};
 
 export const after = afterAll;
 export const before = beforeAll;
