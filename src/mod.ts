d50c71b6773cd58b007d911bc46c57f18a78ece4
Marvin Hagemeister <marvin@deno.com>
2024-04-02 09:52:45 +0200

    feat: add `Buffer` node shim

--- src/mod.ts
+++ src/mod.ts
@@ -1,9 +1,12 @@
 import process from "node:process";
+import { Buffer } from "node:buffer";
 import { afterAll, beforeAll } from "@std/testing/bdd";
 export * from "@std/testing/bdd";
 
 // @ts-ignore node shim
 globalThis.process = process;
+// @ts-ignore node shim
+globalThis.Buffer = Buffer;
 
 export const after = afterAll;
 export const before = beforeAll;
