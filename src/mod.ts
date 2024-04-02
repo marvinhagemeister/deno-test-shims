c0999f9c5a6eec17df42a61c9791b2e413114c8c
Marvin Hagemeister <marvin@deno.com>
2024-04-02 09:51:04 +0200

    feat: add process node shim

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,9 @@
+import process from "node:process";
 import { afterAll, beforeAll } from "@std/testing/bdd";
 export * from "@std/testing/bdd";
+
+// @ts-ignore node shim
+globalThis.process = process;
 
 export const after = afterAll;
 export const before = beforeAll;
